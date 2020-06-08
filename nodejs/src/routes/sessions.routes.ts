a1feaf9fd078ea9fa4b46b4fc1b25cd41c9bca30
Ivan Leal <ivan.leal@alumisoft.com.br>
2020-06-08 17:27:31 -0300

    Include authenticate method

---
+++ nodejs/src/routes/sessions.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import AuthenticateUserService from "../services/AuthenticateUserService";
+
+const sessionsRouter = Router();
+
+sessionsRouter.post("/", async (request, response) => {
+    try {
+
+        const { email, password } = request.body;
+
+        const authenticatUser = new AuthenticateUserService();
+
+        const { user } = await authenticatUser.execute({
+            email,
+            password
+        });
+
+        delete user.password;
+
+        return response.json({ user });
+
+    } catch (error) {
+        return response.status(400).json({ error: error.message });
+    }
+});
+
+export default sessionsRouter;
